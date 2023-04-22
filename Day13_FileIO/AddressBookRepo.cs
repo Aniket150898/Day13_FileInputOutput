156bbf6f95143b6aa3662f6d15e049196fc690a0
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-04-22 21:48:39 +0530

    [Aniket]- Performs Overall FileInput Output Operations

---
+++ Day13_FileIO/AddressBookRepo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_FileIO
+{
+    public class AddressBookRepo
+    {
+        public static AddressBook AddContact()
+        {
+            AddressBook contact = new AddressBook();
+            Console.WriteLine("Enter First name");
+            contact.FirstName=Console.ReadLine();
+            Console.WriteLine("Enter Last name");
+            contact.LastName=Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            contact.Address=Console.ReadLine();
+            Console.WriteLine("Enter City");
+            contact.City=Console.ReadLine();
+            Console.WriteLine("Enter Zip Code");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number");
+            contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Email Address");
+            contact.Email = Console.ReadLine();
+            return contact;
+
+        }
+
+        public static void Display(AddressBook addressBook)
+        {
+            Console.WriteLine(addressBook.FirstName+" "+addressBook.LastName +"\n"+ addressBook.Address + " " + addressBook.City + " " + addressBook.Zip + "\n" + addressBook.PhoneNumber + " " + addressBook.Email);
+        }
+
+        public static AddressBook EditContact(AddressBook addressBook)
+        {
+            Console.WriteLine("Which parameter u want to Edit\n" +
+                "1 first name\n2 Last Name\n3 Address\n4 City\n5 Zip Code\n 6 Phone Number\n 7 Email");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter New First name");
+                    addressBook.FirstName = Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("Enter New Last name");
+                    addressBook.LastName = Console.ReadLine();
+                    break;
+                case 3:
+                    Console.WriteLine("Enter New Address");
+                    addressBook.Address = Console.ReadLine();
+                    break;
+                case 4:
+                    Console.WriteLine("Enter New City");
+                    addressBook.City = Console.ReadLine();
+                    break;
+                case 5:
+                    Console.WriteLine("Enter New Zip Code");
+                    addressBook.Zip = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 6:
+                    Console.WriteLine("Enter New Phone Number");
+                    addressBook.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+                    break;
+                case 7:
+                    Console.WriteLine("Enter New Email");
+                    addressBook.Email = Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine("Please Select Correcct option");
+                    break;
+            }
+            return addressBook;
+
+        }
+        public static void WriteDataToTextFile()
+        {
+            AddressBook addressBook = new AddressBook();
+            addressBook = AddressBookRepo.AddContact();
+            string filePath = @"C:\Users\Aniket\Desktop\Day13\Day13_FileIO\ContactFile.txt";
+            StreamWriter writer = new StreamWriter(filePath, true);
+            writer.WriteLine(addressBook.FirstName);
+            writer.WriteLine(addressBook.LastName);
+            writer.WriteLine(addressBook.Address);
+            writer.WriteLine(addressBook.City);
+            writer.WriteLine(addressBook.Zip);
+            writer.WriteLine(addressBook.PhoneNumber);
+            writer.WriteLine(addressBook.Email);
+            writer.WriteLine(" ");
+            writer.AutoFlush = true;
+        }
+
+        public static void ReadDataToTextFile()
+        {
+            string filePath = @"C:\Users\Aniket\Desktop\Day13\Day13_FileIO\ContactFile.txt";
+            StreamReader reader = new StreamReader(filePath);
+            Console.WriteLine(reader.ReadToEnd()) ;
+        }
+    }
+}
---
+++ Day13_FileIO/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to day 13 File IO system");
+            Console.WriteLine("Select any one option for \n" +
+                "1. Add Contact\n" +
+                "2. Edit Contact\n" +
+                "3. Write Contact to Text file (for UC-13)\n" +
+                "4. Read Data From Text file(for UC-13)");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            { 
+                case 1:
+                    AddressBook addressBook = new AddressBook();
+                    addressBook = AddressBookRepo.AddContact();
+                  break;
+
+                case 2:
+                    AddressBook addressBook1 = new AddressBook();
+                    addressBook1 = AddressBookRepo.AddContact();
+                    AddressBookRepo.Display(addressBook1);
+                    addressBook1=AddressBookRepo.EditContact(addressBook1);
+                    AddressBookRepo.Display(addressBook1);
+                    break;
+                case 3:
+                    AddressBookRepo.WriteDataToTextFile();
+                    break;
+                case 4:
+                    AddressBookRepo.ReadDataToTextFile();
+                    break;
+
+                default:
+                    Console.WriteLine("Select correct option");
+                    break;
+            }
+        }
+    }
+}
